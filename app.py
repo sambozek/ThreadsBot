4c3c949dc01edddd04284aa668faec77181fe433
Sam Bozek <sambozek@gmail.com>
2016-10-16 22:21:18 -0700

    Corrected from 'requests' to Flask's 'request'

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', methods=['Get'])
 def verify():
     """ test to verify that the heroku app is functional. Return errors """
-    if requests.args.get("hub.mode")=='subscribe' and requests.args.get("hub.challenge"):
+    if request.args.get("hub.mode")=='subscribe' and request.args.get("hub.challenge"):
         if not request.args.get('hub.verify_token')== os.environ['VERIFY_TOKEN']:
             return "Token Mismatch", 403
         return request.args["hub.challenge"], 200
