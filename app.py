2cd5d2957bca57ef505599fa2a3df84ba3253b73
Sam Bozek <sambozek@gmail.com>
2016-10-17 18:30:13 -0700

    Setting up messaging conditionals

--- app.py
+++ app.py
@@ -15,3 +15,42 @@ def verify():
         return request.args["hub.challenge"], 200
 
     return "Hello! This is ThreadsBot |X|"
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+
+	data = request.get_json()
+	log(data) # Maintain log for testing
+
+	if data["object"] == 'page':
+		for entry in data['entry']:
+
+			for messaging_event in entry["messaging"]:
+				if messaging_event.get('message'):
+
+					sender_id = messaging_event['sender']['id']
+					recipient_id = messaging_event['sender']['id']
+					message_text = messaging_event['message']['text']
+
+					send_message(sender_id, "cheers, mate")
+
+				if messaging_event.get('delivery'):
+					pass
+
+				if messaging_event.get('optin'):
+					pass
+
+				if messaging_event:
+					pass
+
+	return 'Okie Dokie', 200
+
+
+def send_message(recipient_id, message_text):
+	log('Sent messge to {recipient}: {text}'.format(recipient= recipient_id, text=message_text))
+
+	params = {'access_token' os.environ['PAGE_ACCESS_TOKEN']}
+
+	
+	pass
---
+++ shirt sizes.py
@@ -0,0 +1,140 @@
+['width', 'length']
+
+clothes_dir = {
+	'Abercrombie' : { 
+		'Classic Crew' : {
+			'Small' :  [20.1, 28.1],
+			'Medium' : [21, 28.8],
+			'Large' :  [22.1, 29.2],
+		},
+		'Classic V' : {
+			'Small' :  [19.9, 28.1],
+			'Medium' : [20.8, 28.7],
+			'Large' :  [21.9, 29.1],
+		},
+		'Muscle Crew' : {
+			'Small' :  [20, 26.8],
+			'Medium' : [20.8, 27.2],
+			'Large' :  [21.6, 27.7],
+		},
+		'Muscle V' : {
+			'Small' :  [20, 26.8],
+			'Medium' : [20.8, 27.2],
+			'Large' :  [21.6, 27.7],
+		}, 
+	},
+	'American Apparel' :{
+		'Fine Jersey' : {
+			'Small' :  [17.2, 25.5],
+			'Medium' : [19.1, 26.4],
+			'Large' :  [21, 27.3],
+		},
+		'Fine V' : {
+			'Small' :  [17.2, 25.8],
+			'Medium' : [19.1, 26.3],
+			'Large' :  [21, 27.3],
+		},
+		'Fine Jersey Tall' : {
+			'Small' :  [17.2, 27.2],
+			'Medium' : [19, 28.2],
+			'Large' :  [20.9, 29.3],
+		},
+		'Poly Cotton Jersey' : {
+			'Small' :  [17.4, 26.7],
+			'Medium' : [19.1, 26.3],
+			'Large' :  [21.3, 28.5],
+		},
+		'Poly Cotton Jersey' : {
+			'Small' :  [17.4, 26.7],
+			'Medium' : [19.1, 26.3],
+			'Large' :  [21.3, 28.5],
+		},
+		'Tri-Blend Jersey' : {
+			'Small' :  [17.5, 26.2],
+			'Medium' : [19.5, 27.6],
+			'Large' :  [21.5, 28],
+		} 
+	},
+	'BR' : {
+		'Stretch' : {
+				'Small' :  [18.8, 25.1],
+				'Medium' : [19.8, 26],
+				'Large' :  [21.7, 26.6],
+			},
+		'Cotton Basic' : {
+				'Small' :  [20.4, 25.3],
+				'Medium' : [21.3, 26.3],
+				'Large' :  [22.9, 26.9],
+			},
+		'Basic V' : {
+				'Small' :  [20.4, 25.3],
+				'Medium' : [21.3, 26.3],
+				'Large' :  [22.9, 26.9],
+			},
+	}, 
+	'Fruit of the Loom' : {	
+		'Crew' :{
+				'Small' :  [18.6, 26.9],
+				'Medium' : [20.6, 27.8],
+				'Large' :  [22.6, 28.8],
+			},
+		'V Neck' :{
+				'Small' :  [18.6, 26.9],
+				'Medium' : [20.6, 27.8],
+				'Large' :  [22.6, 28.8],
+			},  
+	},
+	'Gap' : {
+		'Crew' :{
+					'Small' :  [19.5, 25.9],
+					'Medium' : [20.9, 26.7],
+					'Large' :  [22.2, 27.2],
+				},
+		'V Neck' :{
+					'Small' :  [19.5, 25.9],
+					'Medium' : [20.9, 26.7],
+					'Large' :  [22.2, 27.2],
+				}, 
+	},
+	'H&M' : {
+		'Crew' :{
+			'Small' :  [19, 25.5],
+			'Medium' : [20.5, 26.2],
+			'Large' :  [22.1, 27],
+		},
+		'V' :{
+			'Small' :  [19, 25.5],
+			'Medium' : [20.5, 26.2],
+			'Large' :  [22.1, 27],
+		}, 
+	},
+	'Old Navy' :{
+		'Crew' :{
+			'Small' :  [19.6, 25.7],
+			'Medium' : [20.4, 27],
+			'Large' :  [21.9, 27.7],
+		},
+		'V Neck' :{
+			'Small' :  [19.6, 25.7],
+			'Medium' : [20.4, 27],
+			'Large' :  [21.9, 27.7],
+		},
+	},
+	'Uniqlo' :{
+		'Crew' :{
+			'Small' :  [20.1, 26.1],
+			'Medium' : [21.4, 27.2],
+			'Large' :  [23, 28.3],
+		},
+		'V Neck' :{
+			'Small' :  [20.1, 26.1],
+			'Medium' : [21.4, 27.2],
+			'Large' :  [23, 28.3],
+			},
+		'Supima V' :{
+			'Small' :  [20, 25.3],
+			'Medium' : [21.1, 26.5],
+			'Large' :  [22.7, 27.6],
+			},
+	},
+}
